73e6ac76e21c507ef07259d8badc63041f0bff74
Dubnik <ADU089@crb.apmoller.net>
2017-02-17 17:39:00 +0000

    ssl config

--- nginx-waf-docker/start.sh
+++ nginx-waf-docker/start.sh
@@ -6,5 +6,9 @@ test $USE_SSL
 
 /usr/local/bin/confd -onetime -backend env
 
+if [ $USE_SSL == "NO" ]; then
+   rm -rf /usr/local/nginx/conf/nginx.ssl.conf
+fi
+
 echo "Starting Nginx"
 exec nginx -g 'daemon off;' 
